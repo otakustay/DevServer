1cf6d9512bfc8050d1efaa7001ae239dc3943051
otakustay <otakustay@gmail.com>
2011-12-22 16:04:17 +0800

    added request.body

--- server.js
+++ server.js
@@ -17,12 +17,22 @@ config.mapping = config.mapping || {};
 
 function handle(request, response) {
     console.log(request.method + ': ' + request.url);
-    
-    util.extend(request, url.parse(request.url));
-    request.query = request.search ? querystring.parse(request.search.substring(1)) : {};
 
-    var handler = config.mapping[request.url] || config.rescue || res.notFound;
-    handler(request, response);
+    util.extend(request, url.parse(request.url, true));
+    var pathname = (request.pathname.length > 1 && request.pathname.charAt(0) === '/') ? 
+        request.pathname.substring(1) : request.pathname;
+    var handler = config.mapping[pathname] || config.rescue || res.notFound;
+
+    if (handler) {
+        request.body = '';
+        request.setEncoding('utf-8');
+        request.on('data', function(chunk) { request.body += chunk; });
+        request.on('end', function() { handler(request, response); })
+    }
+    else {
+        response.writeHeader(404);
+        response.end();
+    }
 };
 
 var server = http.createServer(handle);
